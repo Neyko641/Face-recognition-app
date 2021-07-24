e83e0cce217fdef158c39a17b6f4d188647daba9
Neyko Naydenov <shendamere@gmail.com>
2021-07-24 19:47:26 +0300

    Face Recognition App in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation  from './components/Navigation/Navigation';
+import ImageLinkForm  from './components/ImageLinkForm/ImageLinkForm';
+import Logo  from './components/Logo/Logo';
+import Rank  from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+
+const paramsOptions = {
+  particles: {
+      number: {
+        value: 30,
+        density: {
+          enable: true,
+          value_area: 800 
+        }
+      }
+    }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation/>
+      <Logo/>
+      <ImageLinkForm />
+      <Rank/>
+      <Particles params={paramsOptions} className='particles'/>
+      {/*<FaceRecognition/>*/}
     </div>
   );
 }
---
+++ src/components/Rank/Rank.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const Rank = () => {
+    return (
+    <div>
+        <div className='white f3'>
+            {`Name, your current Rank is.. `} 
+            {`#5`}
+        </div>
+    </div>
+    );
+}
+export default Rank;
